a4918b01994e2272b64a84dc70ba8b7173a15be3
Sina Sabet <sinasabet81@gmail.com>
2018-02-03 22:57:22 -0800

    fixing header on homepage

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
       <MainContainer>
         <MainHeader>
           <RotatingLogo src={logo} alt="logo" />
-          <MainTitle>crypto-utils</MainTitle>
+          <MainTitle>sec-utils</MainTitle>
         </MainHeader>
         <Passphrase />
         <Encrypter />
